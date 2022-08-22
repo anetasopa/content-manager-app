74abb6a222719437af0ac8ed7f97011e4a83eb20
Bombel <bombel@bombel.com>
2022-08-22 22:26:04 +0200

    Props

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function ComA(props) {
+
+  useEffect(() => {
+    console.log("ComA is using useEffect")
+  }, [props.myProp3])
+
   return(
     <>
       <h1>Hi comA</h1>
@@ -29,34 +34,59 @@ class ComB extends React.Component {
 
   render() {
     const { value } = this.state;
-      
+    const { myProp1, myProp2: MyNewComponent } = this.props;
+
     return (
       <>
         <h1>Hello CompB</h1>
         Current Value: <h1>{ value }</h1>
         <button onClick={() => this.changeState(value + 1)}>+</button>
-        <button onClick={() => this.changeState(value - 1)}>-</button>
+        <button onClick={() => this.changeState(value - 1)}>-</button>  
+        <h2>{myProp1}</h2>
+        <MyNewComponent />
       </>
     )
   }
 }
 
+function MyComponent() {
+  return <h1>My Component</h1>
+}
+
 function Home() {
   const [value, setValue] = useState(10);
+  const [otherValue, setOtherValue] = useState(100);
 
-  console.log(value);
+  useEffect(() => {
+  }, [value])
+
   return (
     <>
       Current Value: <h1>{ value }</h1>
       <button onClick={() => setValue(value+1)}>+</button>
       <button onClick={() => setValue(value-1)}>-</button>
-      <ComA 
+      <hr></hr>
+      Current OtherValue: <h1>{ otherValue }</h1>
+      <button onClick={() => setOtherValue(otherValue+1)}>+</button>
+      <button onClick={() => setOtherValue(otherValue-1)}>-</button>
+      {/* <ComA 
         myProp1={value}
         myProp2={true}
         myProp3="I am comA"
         myProp4={() => <div>My new JSX</div>}
+      /> */}
+      <ComB 
+        myProp1={value}
+        myProp2={
+          () => 
+            <ComA 
+              myProp1={value}
+              myProp2={true}
+              myProp3="I am comA"
+              myProp4={() => <div>My new JSX</div>}
+            />
+        }
       />
-      {/* <ComB /> */}
     </>
   )
 }
